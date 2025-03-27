520b7d04924a1ec69c6f3d673e8dab52beac40e3
Bogdan <bogdantisonok@gmail.com>
2025-03-27 22:09:37 +0400

    Commit

--- lab1.py
+++ lab1.py
@@ -2,6 +2,7 @@ import csv
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 def read_config(config):
     with open(config, newline='') as csvfile:
@@ -18,29 +19,37 @@ def read_config(config):
 def calculate_y(x, a, b, c):
     return a * math.sin(x) + b * math.cos(x) + abs(a * math.sin(x) - b * math.cos(x)) + c
 
-if __name__ == "__main__":
-    xmin, step, xmax, a, b, c = read_config('config.csv')
+xmin, step, xmax, a, b, c = read_config('config.csv')
 
-    x_values = []
-    current_x = xmin
-    while current_x <= xmax:
-        x_values.append(current_x)
-        current_x += step
+x_values = []
+current_x = xmin
+while current_x <= xmax:
+    x_values.append(current_x)
+    current_x += step
 
-    y_values = [calculate_y(x, a, b, c) for x in x_values]
+start_time = time.time()
+y_values = [calculate_y(x, a, b, c) for x in x_values]
+math_time_1 = time.time()
+math_time = math_time_1 - start_time
+print(y_values)
 
-    with open('results.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['x', 'y'])
-        for x, y in zip(x_values, y_values):
-            writer.writerow([x, y])
+x_np = np.arange(2, 12, 0.001)
+start_time_np = time.time()
+y_np = a * np.sin(x_np) + b * np.cos(x_np) + np.abs(a * np.sin(x_np) - b * np.cos(x_np)) + c
+numpy_time = time.time() - start_time_np
 
-    x = np.arange(3, 13, 0.3)
-    print("y = ", a * np.sin(x) + b * np.cos(x) + abs(a * np.sin(x) - b * np.cos(x)) + c)
+with open('results.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['x', 'y (numpy)'])
+    for x, y in zip(x_np, y_np):
+        writer.writerow([x, y])
 
-    plt.plot(x_values, y_values, marker = "o")
-    plt.title('График функции y(x)')
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.grid()
-    plt.show()
+print(f"Затрачено времени math: {math_time} сек")
+print(f"Затрачено времени numpy: {numpy_time} сек")
+
+plt.plot(x_values, y_values, marker = "o")
+plt.title('График функции y(x)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid()
+plt.show()
